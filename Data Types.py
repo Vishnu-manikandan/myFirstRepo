a5dd7337f4356ba016f7e46cecbf3553ff367515
Vishnu-manikandan <133263115+Vishnu-manikandan@users.noreply.github.com>
2023-05-12 00:54:17 +0530

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,8 @@
+#Data Types
+a=10 #integer or whole numbers
+b="hi" #Strings
+c="73" #string
+a=77.2 #Float
+a=True#Boolean
+b=False
+b='f' #string
---
+++ Task 1.py
@@ -0,0 +1,4 @@
+year=int(input('Enter your Birth date'))
+age=2023-year
+print('You are born in : ',year)
+print('In Year 2023,you will be ',age,' Years old')
